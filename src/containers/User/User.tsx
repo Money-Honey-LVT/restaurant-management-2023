3b7ae4045412631599ffb5de338490532661d0ea
thanhtran17 <enigma15012001@gmail.com>
2023-04-04 09:35:30 +0700

    list account

--- src/containers/User/User.tsx
+++ src/containers/User/User.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { IconChevronRight, IconChevronLeft } from '@tabler/icons-react';
-import { UnstyledButton, Group, Avatar, Text, Box, useMantineTheme, rem } from '@mantine/core';
+import { IconChevronRight, IconChevronLeft, IconSettings, IconMessageCircle, IconPhoto } from '@tabler/icons-react';
+import { UnstyledButton, Group, Avatar, Text, Box, useMantineTheme, rem, Menu } from '@mantine/core';
 
 const User = () => {
   const theme = useMantineTheme();
@@ -25,22 +25,32 @@ const User = () => {
           },
         }}
       >
-        <Group>
-          <Avatar
-            src="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80"
-            radius="xl"
-          />
-          <Box sx={{ flex: 1 }}>
-            <Text size="sm" weight={500}>
-              Nguyễn Văn A
-            </Text>
-            <Text color="dimmed" size="xs">
-              Quản lý nhà hàng
-            </Text>
-          </Box>
+        <Menu position="right-end">
+          <Menu.Target>
+            <Group>
+              <Avatar
+                src="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80"
+                radius="xl"
+              />
+              <Box sx={{ flex: 1 }}>
+                <Text size="sm" weight={500}>
+                  Nguyễn Văn A
+                </Text>
+                <Text color="dimmed" size="xs">
+                  Quản lý nhà hàng
+                </Text>
+              </Box>
 
-          {theme.dir === 'ltr' ? <IconChevronRight size={rem(18)} /> : <IconChevronLeft size={rem(18)} />}
-        </Group>
+              <IconChevronRight size={rem(18)} />
+            </Group>
+          </Menu.Target>
+
+          <Menu.Dropdown>
+            <Menu.Item icon={<IconSettings size={14} />}>Settings</Menu.Item>
+            <Menu.Item icon={<IconMessageCircle size={14} />}>Messages</Menu.Item>
+            <Menu.Item icon={<IconPhoto size={14} />}>Gallery</Menu.Item>
+          </Menu.Dropdown>
+        </Menu>
       </UnstyledButton>
     </Box>
   );
