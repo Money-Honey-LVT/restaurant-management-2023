55382e2aeb34373bd4ec3bfe20b82150f9e29909
thanhtran17 <enigma15012001@gmail.com>
2023-04-06 17:18:56 +0700

    prettier config

---
+++ src/components/Tables/EditTableModal/EditTableModal.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const EditTableModal = () => {
+  return <div>EditTableModal</div>;
+};
+
+export default EditTableModal;
--- src/components/Tables/TableCard/TableCard.tsx
+++ src/components/Tables/TableCard/TableCard.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import { Table } from '../../../types/models/table';
+import { ActionIcon, Badge, Card, Group, Menu, Modal, Text, rem } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import { modals } from '@mantine/modals';
-import { ActionIcon, Badge, Button, Card, Group, Menu, Text, rem } from '@mantine/core';
 import { notifications } from '@mantine/notifications';
 import { IconCheck, IconDots, IconEdit, IconTrash } from '@tabler/icons-react';
+import React from 'react';
+import { Table } from '../../../types/models/table';
+import EditTableModal from '../EditTableModal';
 
 interface Props {
   item: Table | null;
 }
 
 const TableCard: React.FC<Props> = ({ item }) => {
+  const [editOpened, { close: closeEditModal, open: openEditModal }] = useDisclosure();
+
   const openDeleteTableModal = () =>
     modals.openConfirmModal({
       title: 'Xác Nhận Xoá Bàn',
@@ -37,47 +41,42 @@ const TableCard: React.FC<Props> = ({ item }) => {
         }),
     });
 
-  const openEditTableModal = () =>
-    modals.openConfirmModal({
-      title: 'Sửa Thông Tin Bàn',
-      centered: true,
-      children: <Text size="sm"></Text>,
-      labels: { confirm: 'Đồng ý', cancel: 'Huỷ bỏ' },
-      onCancel: () => console.log('Cancel'),
-      onConfirm: () => console.log('Confirmed'),
-    });
-
   return (
-    <Card shadow="sm" padding="lg" radius="md" withBorder>
-      <Card.Section withBorder inheritPadding py="xs">
-        <Group position="apart">
-          <Text weight={500}>Bàn {item?.name}</Text>
-          <Menu withinPortal position="bottom-end" shadow="sm">
-            <Menu.Target>
-              <ActionIcon>
-                <IconDots size="1rem" />
-              </ActionIcon>
-            </Menu.Target>
+    <>
+      <Card shadow="sm" padding="lg" radius="md" withBorder>
+        <Card.Section withBorder inheritPadding py="xs">
+          <Group position="apart">
+            <Text weight={500}>Bàn {item?.name}</Text>
+            <Menu withinPortal position="bottom-end" shadow="sm">
+              <Menu.Target>
+                <ActionIcon>
+                  <IconDots size="1rem" />
+                </ActionIcon>
+              </Menu.Target>
 
-            <Menu.Dropdown>
-              <Menu.Item onClick={openEditTableModal} icon={<IconEdit size={rem(14)} />}>
-                Sửa thông tin
-              </Menu.Item>
-              <Menu.Item onClick={openDeleteTableModal} icon={<IconTrash size={rem(14)} />} color="red">
-                Xoá bàn
-              </Menu.Item>
-            </Menu.Dropdown>
-          </Menu>
+              <Menu.Dropdown>
+                <Menu.Item onClick={openEditModal} icon={<IconEdit size={rem(14)} />}>
+                  Sửa thông tin
+                </Menu.Item>
+                <Menu.Item onClick={openDeleteTableModal} icon={<IconTrash size={rem(14)} />} color="red">
+                  Xoá bàn
+                </Menu.Item>
+              </Menu.Dropdown>
+            </Menu>
+          </Group>
+        </Card.Section>
+
+        <Group position="apart" mt="md" mb="xs">
+          <Text>{item?.capacity} chỗ ngồi</Text>
+          <Badge color="pink" size="lg" variant="light">
+            {item?.status?.toUpperCase()}
+          </Badge>
         </Group>
-      </Card.Section>
-
-      <Group position="apart" mt="md" mb="xs">
-        <Text>{item?.capacity} chỗ ngồi</Text>
-        <Badge color="pink" size="lg" variant="light">
-          {item?.status?.toUpperCase()}
-        </Badge>
-      </Group>
-    </Card>
+      </Card>
+      <Modal centered opened={editOpened} onClose={closeEditModal} title="Sửa Thông Tin Bàn">
+        <EditTableModal />
+      </Modal>
+    </>
   );
 };
 
