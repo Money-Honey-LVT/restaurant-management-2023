0cba055bf9d5f2cec25eb68af633196bdc631f6e
thanhtran17 <enigma15012001@gmail.com>
2023-05-05 21:48:36 +0700

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -6,30 +6,6 @@ import dns from 'dns';
 dns.setDefaultResultOrder('verbatim');
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    plugins: [svgr(), react()],
-    define: {
-      'process.env': process.env,
-    },
-    // resolve: {
-    //   alias: {
-    //     "@": fileURLToPath(new URL("./src", import.meta.url)),
-    //   },
-    // },
-    server: {
-      host: 'localhost',
-      port: +env.VITE_PORT,
-      open: true,
-      proxy: {
-        '/dbcl': {
-          target: env.VITE_APP_URL,
-          secure: false,
-          changeOrigin: true,
-        },
-      },
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
 });
